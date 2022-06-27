ae66eae139395e090a1bbab1501cf0e04dacc133
MateuszSkupien <mateuszskupien.ti@gmail.com>
2022-06-27 15:31:55 +0200

    Main clean up

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@
 int main(){
 	Control app(false);
 	app.addTexture("res/graphics/bg.png",0,0);
-	// app.addTexture("res/graphics/hero.png",100,100);
-	// app.addTexture("res/graphics/hero.png",200,200);
-	// app.addTexture("res/graphics/hero.png",300,300);
-	// app.addTexture("res/graphics/hero.png",400,400);
 	Barnim::Hero Hero;
 	Hero.LoadTexture("res/graphics/hero.png",200,200,0,0);
 	app.addDrawableObject(&Hero);
